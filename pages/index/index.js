7b914f4e4fb19fb9f34f3f91d09a1081adf7fd2b
赵慧杰 <zhaohuijie@jd.com>
2018-06-07 18:48:13 +0800

    注册页面生命周期和相关事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,41 @@ const app = getApp();
 
 Page({
     data: {
-        motto: 'Hello World',
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
+        text: 'init data',
+        array: [{ msg: '1' }, { msg: '2' }]
     },
-    //事件处理函数
-    bindViewTap: function() {
-        wx.navigateTo({
-            url: '../logs/logs'
-        });
+    // 页面加载 一个页面只会调用一次，可以在 onLoad 中获取打开当前页面所调用的 query 参数
+    onLoad: function(query) {
+        console.log('onLoad');
     },
-    onLoad: function() {
-        if (app.globalData.userInfo) {
-            this.setData({
-                userInfo: app.globalData.userInfo,
-                hasUserInfo: true
-            });
-        } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                });
-            };
-        } else {
-            // 在没有 open-type=getUserInfo 版本的兼容处理
-            wx.getUserInfo({
-                success: res => {
-                    app.globalData.userInfo = res.userInfo;
-                    this.setData({
-                        userInfo: res.userInfo,
-                        hasUserInfo: true
-                    });
-                }
-            });
-        }
+    // 页面初次渲染完成 一个页面只会调用一次，代表页面已经准备妥当，可以和视图层进行交互。 对界面的设置如wx.setNavigationBarTitle请在onReady之后设置
+    onReady: function() {
+        console.log('onReady');
     },
-    getUserInfo: function(e) {
-        console.log(e);
-        app.globalData.userInfo = e.detail.userInfo;
-        this.setData({
-            userInfo: e.detail.userInfo,
-            hasUserInfo: true
-        });
+    // 页面显示 每次打开页面都会调用一次
+    onShow: function() {
+        console.log('onShow');
+    },
+    // 页面隐藏 当navigateTo或底部tab切换时调用
+    onHide: function() {
+        console.log('onHide');
+    },
+    // 页面卸载 当redirectTo或navigateBack的时候调用
+    onUnload: function() {
+        console.log('onUnload');
+    },
+    // 下拉刷新
+    // 监听用户下拉刷新事件。
+    // 需要在app.json的window选项中或页面配置中开启enablePullDownRefresh。
+    // 当处理完数据刷新后，wx.stopPullDownRefresh可以停止当前页面的下拉刷新
+    onPullDownRefresh: function() {
+        console.log('下拉刷新');
+    },
+    // 上拉触底
+    // 监听用户上拉触底事件。
+    // 可以在app.json的window选项中或页面配置中设置触发距离onReachBottomDistance。
+    // 在触发距离内滑动期间，本事件只会被触发一次
+    onReachBottom: function() {
+        console.log("上拉触底");
     }
 });
