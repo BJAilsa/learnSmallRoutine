34c1f58b6563a136fbd086327fb512c09c3210e5
赵慧杰 <zhaohuijie@jd.com>
2018-06-08 15:55:34 +0800

    生命周期函数，页面相关事件处理函数，公用类库调用方法

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,26 @@
 //index.js
 //获取应用实例
 const app = getApp();
+let util = require('../../utils/util.js');
 
 Page({
     data: {
         text: 'init data',
-        array: [{ msg: '1' }, { msg: '2' }]
+        num: 0,
+        array: [{ text: 'init data' }],
+        object: {
+            text: 'init data'
+        }
     },
+    /**
+     *
+     *生命周期函数
+     *
+     */
     // 页面加载 一个页面只会调用一次，可以在 onLoad 中获取打开当前页面所调用的 query 参数
     onLoad: function(query) {
         console.log('onLoad');
+        console.log(wx.getSystemInfoSync);
     },
     // 页面初次渲染完成 一个页面只会调用一次，代表页面已经准备妥当，可以和视图层进行交互。 对界面的设置如wx.setNavigationBarTitle请在onReady之后设置
     onReady: function() {
@@ -27,6 +38,11 @@ Page({
     onUnload: function() {
         console.log('onUnload');
     },
+    /**
+     *
+     * 页面相关事件处理函数
+     *
+     */
     // 下拉刷新
     // 监听用户下拉刷新事件。
     // 需要在app.json的window选项中或页面配置中开启enablePullDownRefresh。
@@ -39,6 +55,60 @@ Page({
     // 可以在app.json的window选项中或页面配置中设置触发距离onReachBottomDistance。
     // 在触发距离内滑动期间，本事件只会被触发一次
     onReachBottom: function() {
-        console.log("上拉触底");
+        console.log('上拉触底');
+    },
+    // 监听用户滑动页面事件
+    // 参数为Object,包含以下字段scrollTop 页面在垂直方向已滚动的距离（单位px）
+    onPageScroll: function() {
+        console.log('页面滚动');
+    },
+    // 用户转发
+    // 只有定义了此事件处理函数，右上角菜单才会显示“转发”按钮
+    // 用户点击转发按钮的时候会调用
+    // 此事件需要 return 一个 Object，用于自定义转发内容
+    onShareAppMessage: function() {
+        return {
+            title: '自定义转发标题',
+            path: '/page/user?id=123'
+        };
+    },
+    /**
+     *
+     * 事件处理函数
+     *
+     */
+    changeText: function() {
+        // this.data.text = 'changed data' //bad, it can not work
+        this.setData({
+            text: 'changed data'
+        });
+    },
+    changeNum: function() {
+        this.data.num = 1;
+        this.setData({
+            num: this.data.num
+        });
+    },
+    changeItemInArray: function() {
+        // you can use this way to modify a danamic data path
+        this.setData({
+            'array[0].text': 'changed data'
+        });
+    },
+    changeItemInObject: function() {
+        this.setData({
+            'object.text': 'changed data'
+        });
+    },
+    addNewField: function() {
+        this.setData({
+            'newField.text': 'new data'
+        });
+    },
+    helloMINA: function(){
+        util.sayHello('MINA');
+    },
+    goodbyeMINA: function(){
+        util.sayGoodbye('MINA');
     }
 });
--- utils/util.js
+++ utils/util.js
@@ -1,23 +1,10 @@
-const formatTime = date => {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
-
-    return (
-        [year, month, day].map(formatNumber).join('/') +
-        ' ' +
-        [hour, minute, second].map(formatNumber).join(':')
-    );
-};
-
-const formatNumber = n => {
-    n = n.toString();
-    return n[1] ? n : '0' + n;
-};
-
+function sayHello(name){
+    console.log(`Hello ${name}!`);
+}
+function sayGoodbye(name){
+    console.log(`Goodbye ${name}`);
+}
 module.exports = {
-    formatTime: formatTime
+    sayHello: sayHello,
+    sayGoodbye: sayGoodbye
 };
