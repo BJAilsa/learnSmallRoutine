1d49aac9e7341b40a5d0d3531d8e3101fb0cba59
赵慧杰 <zhaohuijie@jd.com>
2018-06-12 18:40:18 +0800

    wxml语法学习

---
+++ pages/wxmlg/wxmlg.js
@@ -0,0 +1,12 @@
+Page({
+    data: {
+        message: 'Hello MINA!',
+        id: 0,
+        condition: true,
+        flag: false,
+        a: 1,
+        b: 2,
+        c: 3,
+        length:6
+    }
+})
